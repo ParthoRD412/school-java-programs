0223761055c8a53369eecf73ebe11e034c0a9187
ParthoRD412 <85815230+ParthoRD412@users.noreply.github.com>
2022-02-07 09:54:36 +0530

    changes have been made to the code and the algo

--- emrip_number/source-code.java
+++ emrip_number/source-code.java
@@ -14,12 +14,16 @@ public class Emirp{//Class declaration
         if (isPrime(n) == 0)//isPrimemethod called to check whether the number is actually a prime number or not.
             System.out.println("Not Emirp number");//In case the number is composite
         int rev = 0;//Variable ro store reverse of the number
+        int copy = n;//Making a copy of the variable n
         while (n != 0) {//Number reversing algorithm by going through each digit from the end
             int d = n % 10;//Digit extraction
             rev = rev * 10 + d;//Reverse number updated
             n /= 10;//Number updated i.e. 1 digit reduced
         }
+        if(rev ==  copy)
         System.out.print("Yes Emirp number");//If number is prime and rev = number then its an emirp number
+        else
+        System.out.println("Not Emrip number");
     }
     public static void main(String args[]){//Main method to call all the functions
         Scanner sc=new Scanner(System.in);//Scanner instance
